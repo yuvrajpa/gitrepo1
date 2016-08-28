bcda7d7ed6aee92184cba2f0a9dc34c2227c8b84
yuvrajpa <patil.anu11@gmail.com>
2016-08-28 17:03:31 +0530

    maven

---
+++ firstgit/src/main/java/gittrial/App.java
@@ -0,0 +1,13 @@
+package gittrial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ firstgit/src/test/java/gittrial/AppTest.java
@@ -0,0 +1,16 @@
+package gittrial;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class AppTest{
+	@Test
+	public void trial(){
+		/*WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.firstpost.com");
+		System.out.println("anu");*/
+		
+		System.out.println("anu");
+	}
+}
